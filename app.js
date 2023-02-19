4ace4f5660a57524b4cdc2a76e77e6c4c40f8ced
Nahelool <yahel300yaffe@gmail.com>
2023-02-19 20:43:11 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var fs = require('fs')
+const port = 81
+
+
+var server = http.createServer((req, res) => {
+    res.write("dasdasd")
+    fs.readFile('indox.html', function(error,data){
+        if (error){
+            res.writeHead(404)
+            res.write('Error : File Not Found')
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html-'})
+            res.write(data)
+        }
+    })
+    res.end()
+});
+
+server.listen(port, function(error){
+    if (error){
+        console.log("Something went wrong", error)
+    } else {
+        console.log("Server is listening at port " + port)
+    }
+})
