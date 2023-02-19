822fc424766ab2c6c93d6b241d13cb2f242278fa
Nahelool <yahel300yaffe@gmail.com>
2023-02-19 23:53:49 +0200

    HTML Reading Modified

--- app.js
+++ app.js
@@ -4,17 +4,16 @@ const port = 3000
 
 
 var server = http.createServer((req, res) => {
-    res.write("dasdasd")
-    fs.readFile('indox.html', function(error,data){
+    fs.readFile('Script/index.html', function(error,data){
         if (error){
             res.writeHead(404)
             res.write('Error : File Not Found')
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html-'})
+            res.writeHead(200, { 'Content-Type': 'text/html'})
             res.write(data)
         }
-    })
     res.end()
+    })
 });
 
 server.listen(port, function(error){
