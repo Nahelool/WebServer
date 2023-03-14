02c17d9672bd9c98ee2586faf81eee502e669e7e
Nahelool <yahel300yaffe@gmail.com>
2023-03-14 21:27:24 +0200

    14.3 Yahel

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-var http = require('http')
+
+const express = require('express')
+const app = express()
 var fs = require('fs')
-const port = 81
 
+const port = 3000
 
-var server = http.createServer((req, res) => {
-    fs.readFile('Script/index.html', function(error,data){
-        if (error){
-            res.writeHead(404)
-            res.write('Error : File Not Found')
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html'})
-            res.write(data)
-        }
-    res.end()
-    })
-});
-
-server.listen(port, function(error){
-    if (error){
-        console.log("Something went wrong", error)
-    } else {
-        console.log("Server is listening at port " + port)
-    }
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// var server = http.createServer((req, res) => {
+//     fs.readFile('HTML/index.html', function(error,data){
+//         if (error){
+//             res.writeHead(404)
+//             res.write('Error : File Not Found')
+//         } else {
+//             res.writeHead(200, { 'Content-Type': 'text/html'})
+//             res.write(data)
+//         }
+//         fs.readFile('HTML/stniggere.css', function(error,style){
+//             if (error){
+//                 res.writeHead(404)
+//                 res.write('Error : File Not Found')
+//             } else {
+//                 res.writeHead(200, { 'Content-Type': 'text/css'})
+//                 console.log("Hello")
+//                 res.write(style)
+//             }
+//         })  
+//     res.end()
+//     })
+// });
+
+// server.listen(port, function(error){
+//     if (error){
+//         console.log("Something went wrong", error)
+//     } else {
+//         console.log("Server is listening at port " + port)
+//         console.log("567")
+//     }
+// })
