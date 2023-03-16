0351c6fb78c6f41f349d351b1d57cba9384267a6
Nahelool <yahel300yaffe@gmail.com>
2023-03-16 21:05:10 +0200

    16.3 Yahel

--- server/server.js
+++ server/server.js
@@ -1,61 +1,42 @@
 
 var express = require('express')
-
 var app = express()
 var http = require('http')
 http.createServer(app)
-var io = require('socket.io')(http)
+// var io = require('socket.io')(http)
 var fs = require('fs')
-const { Socket } = require('socket.io-client')
-const { response } = require('express')
-const { url } = require('inspector')
+var url = require('url')
 
+// const { Socket } = require('socket.io-client')
+// const { response } = require('express')
+// const { url } = require('inspector')
+// const { send } = require('process')
 
-var clientResponseRef
+// var clientResponseRef
+
 app.use(express.static('../Public'))
 
-app.get('/*', (req, res) => {
-  console.log("Established connection")
-  // req = Request , res = Response
-  // var pathName = url.parse(req.url).pathName
-  // //Gets the asked path of file to fetch
-  // var obj ={
-  //   pathname : pathname,
-  //   method: "get",
-  //   params: req.query
-  // }
-  // io.emit("page-request", obj)
-  // clientResponseRef = res
+notFound = fs.readFileSync("../Public/HTML/404.html")
+index = fs.readFileSync("../Public/HTML/index.html")
 
-  res.writeHead(200,{'Content-Type':"text/html"})
-  html = fs.readFileSync("../Public/HTML/index.html")
-  // html2 = fs.readFileSync("multipart/form-data"; boundary=something)
-  // res.write(html2)
-  res.end(html)
-
-
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type':'text/html'})
+  res.write(index)
+  res.end()
 })
 
-
-// app.post('/*', (req, res) => {
-//   // req = Request , res = Response
-//   var pathName = url.parse(req.url).pathName
-//   //Gets the asked path of file to fetch
-//   var obj ={
-//     pathname : pathname,
-//     method: "post",
-//     params: req.body
-//   }
-//   io.emit("page-request", obj)
-//   clientResponseRef = res
-// })
-
-// io.on('connection', (socket) =>{
-//   console.log('A node connected')
-//   socket.on('page-response', (response)=>{
-//     clientResponseRef.send(response)
-//   })
-// })
+app.get('/*', (req, res) => {
+  fs.readFile('../Public/HTML'+req.url+'.html', function(error,data){
+    if(error){
+        res.writeHead(404, {'Content-Type':'text/html'})
+        res.write(notFound)
+    } else {
+      res.writeHead(200, {'Content-Type':'text/html'})
+      res.write(data)
+    }
+    res.end()
+  })
+})
 
 var serverPort = process.env.YOUR_PORT || process.env.PORT || 3000
 
