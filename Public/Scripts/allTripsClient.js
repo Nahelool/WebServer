35decea3a0573de6dbf140af044744d89b94de16
GigllyBoo <yonatanhorwitz24@gmail.com>
2024-04-09 10:59:11 +0300

    Yonatan 9.4

--- Public/Scripts/allTripsClient.js
+++ Public/Scripts/allTripsClient.js
@@ -1,6 +1,6 @@
 window.onload = async ()=>  {
     let tripTemp = document.getElementById("tripTemp")
-    let tripDiv = tripTemp.content.querySelector("div");
+    let tripDiv = tripTemp.content.getElementByClassName("text-cont");
 
     const tripArray = await fetch(serverAdress+"allTrips", {method: 'GET'});
     tripArray.forEach(trip => {
@@ -9,10 +9,10 @@ window.onload = async ()=>  {
         let arrivalTime = trip.Return
 
         let div = document.importNode(tripDiv,true)
-        div.content.getElementById("Animal_ID").textContent = Animal_ID
-        div.content.getElementById("leaveTime").textContent = leaveTime
-        div.content.getElementById("arrivalTime").textContent = arrivalTime
-        document.body.appendChild(div)
+        div.content.getElementByClassName("dog-name").textContent = Animal_ID
+        div.content.getElementByClassName("dog-walks-start").textContent = leaveTime
+        div.content.getElementByClassName("dog-walks-end").textContent = arrivalTime
+        document.getElementsByClassName("dog-walks-cont").appendChild(div)
     });
 
   }
