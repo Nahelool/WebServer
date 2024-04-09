b72321ec3e781dd5e879a839604274a9ccff83be
GigllyBoo <yonatanhorwitz24@gmail.com>
2024-04-09 10:23:01 +0300

    Yonatan 4.8 managerPage

--- Public/Scripts/profileLoad.js
+++ Public/Scripts/profileLoad.js
@@ -1,6 +1,6 @@
 
 const firstName = document.getElementById('firstName')
-const Volunteer_ID = document.getElementById("Volunteer_IDs")
+const Volunteer_ID = document.getElementById("Volunteer_ID")
 const gmail = document.getElementById("gmail")
 const age = document.getElementById("age")
 const phoneNumber = document.getElementById("phoneNumber")
@@ -8,10 +8,10 @@ window.onload = ()=>{
   if(!sessionStorage.userData){
     location.href = '/'
   } else {
-    firstName.innerHTML = sessionStorage.userData.name
-    Volunteer_ID.innerHTML = sessionStorage.userData.Volunteer_ID
-    gmail.innerHTML = sessionStorage.userData.gmail
+    firstName.innerHTML = sessionStorage.name
+    Volunteer_ID.innerHTML = sessionStorage.Volunteer_ID
+    gmail.innerHTML = sessionStorage.gmail
     age.innerHTML = sessionStorage.age
-    phoneNumber.innerHTML = sessionStorage.userData.phoneNumber
+    phoneNumber.innerHTML = sessionStorage.phoneNumber
   }
 }
