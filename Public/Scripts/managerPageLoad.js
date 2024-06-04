3662d7c3bc9082a0503c9f43bb5c92a6e342b563
GigllyBoo <yonatanhorwitz24@gmail.com>
2024-06-04 12:11:43 +0300

    yonatan even more changes

--- Public/Scripts/managerPageLoad.js
+++ Public/Scripts/managerPageLoad.js
@@ -6,7 +6,20 @@ window.onload = () =>{
     sessionStorage.voulenteers = data.voulenteersArray
   })
 }
+let voulenteerArrival = document.querySelector(".missionCont");
+let Text = dogTrip.content.querySelector(".missionText")
+
 voulenteersTimeArray = sessionStorage.voulenteers
-voulenteersTimeArray.forEach(voulenteer => {
-    
+for (let i = 0; index < 7; index++) {
+    dayArray = []
+    voulenteersTimeArray.forEach(voulenteer => {
+        let voulenteer_ID = voulenteersTimeArray.Voulenteer_ID
+        let arrivalTime = voulenteersTimeArray.arrivalTime
+        if(arrivalTime.day == i){
+            dayArray.push({voulenteer_ID})
+            dayArray.push({arrivalTime})
+        } 
+        td.querySelector[i](".voulenteerList").textContent = dayArray;
+
 });
+}
