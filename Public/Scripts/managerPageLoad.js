34a384a3dc1ff685df867f8a11a58693bc9868d5
Nahelool <yahel300yaffe@gmail.com>
2024-06-08 17:49:57 +0300

    8.6 Yahel

--- Public/Scripts/managerPageLoad.js
+++ Public/Scripts/managerPageLoad.js
@@ -6,20 +6,20 @@ window.onload = () =>{
     sessionStorage.voulenteers = data
   })
 }
-let voulenteerArrival = document.querySelector(".missionCont");
-let Text = dogTrip.content.querySelector(".missionText")
+
+const tds = document.querySelectorAll(".voulenteerList")
 
 voulenteersTimeArray = sessionStorage.voulenteers
 for (let i = 0; index < 7; index++) {
     dayArray = []
     voulenteersTimeArray.forEach(voulenteer => {
-        let voulenteer_ID = voulenteersTimeArray.Voulenteer_ID
-        let arrivalTime = voulenteersTimeArray.arrivalTime
+        let voulenteer_ID = voulenteer[0]
+        let arrivalTime = voulenteer[1]
         if(arrivalTime.day == i){
             dayArray.push({voulenteer_ID})
             dayArray.push({arrivalTime})
         } 
-        td.querySelector[i](".voulenteerList").textContent = dayArray;
+        tds[i].textContent = dayArray;
 
 });
 }
